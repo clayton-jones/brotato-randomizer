7734e2a514b99b4751570634d4db6959fdab0414
Clayton Jones <claytonjoneslmp@gmail.com>
2023-12-23 21:43:07 -0800

    reading directory of image files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,10 @@
-import CharacterTile from "./components/character-tile"
+// import CharacterTile from "./components/character-tile"
+import fs from 'fs/promises'
 
-export default function Home() {
+export default async function Home() {
+    const image_array = await fs.readdir("./public/characters")
+    image_array.shift()
+
   return (
     <main>
       <h1 id="page-title">Brotato Randomizer</h1>
